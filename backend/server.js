339bc3de0fe876baf86bb901023dded2b2fe87c6
Bryan <jngregorio1@up.edu.ph>
2024-05-14 02:36:04 +0800

    feat: added models userSchema and login/register get and post methods

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,17 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const User = require('./models/userSchema');
 
-
+const SECRET_KEY = 'secretkey';
 
 //connect to express server
 const app = express();
 
 
 //connect to mongodb
+//create a .env
+             //mongodb+srv://jngregorio1:bryan22@cluster5173.afcyvk6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster5173
 const dbURI = 'mongodb+srv://jngregorio1:bryan22@cluster5173.afcyvk6.mongodb.net/UsersDB?retryWrites=true&w=majority&appName=Cluster5173'
 mongoose
 .connect(dbURI, {
@@ -28,10 +31,58 @@ mongoose
 
 
 
-//middlenames
+//middleware
+app.use(bodyParser.json())
+app.use(cors())
 
-
-//schema
 
 
 //routes
+//USER REGISTER
+//POST REGISER
+app.post('/register', async (req, res) => {
+    try {
+        const {firstname, lastname, username, phone, email, password} = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({firstname, lastname, username, phone, email, password: hashedPassword});
+        await newUser.save();
+        res.status(201).json({message: 'Registration successful'});
+    } catch (error) {
+        console.error("Registration error:", error);
+        res.status(500).json({error: 'Error registering', details: error.message});
+    }
+});
+
+//GET REGISTERED USERS
+//DONT RETURN PASSWORD AND NOT ALL USERS
+app.get('/register', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.status(201).json(users)
+    }catch (error) {
+        res.status(500).json({error: 'Error unable to get users'})
+    }
+})
+
+//GET LOGIN USERS
+app.post('/login', async (req, res) => {
+    try{
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(401).json({error: 'Invalid credentials'})
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if(!isPasswordValid){
+            return res.status(401).json({error: 'Invalid credentials'})
+        }
+        const token = jwt.sign({userId: user._id}, SECRET_KEY, {expiresIn: '1hr'})
+        res.json({message: "Login successfull"})
+    } catch(error){
+        res.status(500).json({error: "Error signing in"})
+    }
+})
+
+
+//Create
+//Read
