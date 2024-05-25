2e85637628c23cb5e581c3f13ac0c372027a9dab
riggs249 <rttomas@up.edu.ph>
2024-05-25 16:49:43 +0800

    chore: updated the model for user to display phone number properly, and modified the user details being displayed

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     lastname: { type: String, required: true},
     username: {type: String, required: true, unique: true},
     middlename:{type: String, required: false},
-    phone: {type: Number, required: true},
+    phone: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     userType: {type: String, required: true, default: 'user'},
